88978a837dc83ff185f530630cab2657fbade28d
littlec5201 <cjbensemann@gmail.com>
2017-07-12 15:28:53 +1200

    Entry/exit times are now able to be logged in the database
    When a student has their tag read by an RFID reader, their status in the
    database is now updated regardless of the time the tag was read. Further
    checks are now made to determine whether a student has arrived late or
    departed early. Parents are now notified if either of these criteria are
    met via email and are asked to log into the web application to provide
    an explanation.

--- SerialCommunication/src/ClassModel/SerialCommunication.java
+++ SerialCommunication/src/ClassModel/SerialCommunication.java
@@ -6,6 +6,9 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,6 +22,11 @@ public class SerialCommunication {
 
     private Connection connection;
     private Statement statement;
+
+//    The start and finish time of the school
+    private final String startTime = "09:00";
+    private final String finishTime = "15:00";
+    private final int leniance = 15;
 
     /**
      * The default constructor creates a new DBConnection object, which contains
@@ -34,6 +42,7 @@ public class SerialCommunication {
      * associated with the tagKey that one of the RFID readers detected.
      *
      * @param tagKey the tagKey stored on the RFID card scanned by a reader
+     * @return whether the update was successful or not
      */
     public boolean updatePresentStatus(String tagKey) {
         try {
@@ -68,11 +77,13 @@ public class SerialCommunication {
              */
             statement.execute("UPDATE student SET present = " + updatedValue + " WHERE studentID = " + studentID);
             System.out.println(name + " status updated");
-            sendEmail(tagKey);
+//            sendEmail(tagKey);
+            updateAttendance(studentID, updatedValue, tagKey);
             return true;
         } catch (SQLException ex) {
             /**
-             * The card that a reader detected was either read incorrectly or the rfidKey does not exist in the database.
+             * The card that a reader detected was either read incorrectly or
+             * the rfidKey does not exist in the database.
              */
             System.out.println("Card not recognised");
         }
@@ -80,8 +91,160 @@ public class SerialCommunication {
     }
 
     /**
-     * Sends an email to the parents of the student associated with the tagKey identified.
-     * 
+     * This method will either create a new entry in the attendance table or
+     * update an already existing row depending on whether the student is
+     * logging in or out.
+     *
+     * @param studentID the studentID of the student whose attendance is being
+     * updated
+     * @param currentStatus the binary value that determines whether a student
+     * is in school or not
+     * @param tagKey the tagkey relating to the student
+     * @return whether the attendance records were successfully updated or not
+     */
+    public boolean updateAttendance(int studentID, int currentStatus, String tagKey) {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/YYYY");
+        DateFormat timeFormat = new SimpleDateFormat("HH:mm");
+        Date date = new Date();
+        String logDate = dateFormat.format(date);
+        String logTime = timeFormat.format(date);
+
+        boolean addToExcuses = false;
+
+        if (currentStatus == 1) {
+            try {
+                /**
+                 * Creates a new attendance record in the attendance table.
+                 */
+                statement.execute("INSERT INTO attendance (attendanceID, studentID, date, signInTime, signOutTime)"
+                        + "VALUES (NULL, " + studentID + ", '" + logDate + "', '" + logTime + "', NULL)");
+
+                /**
+                 * Check to see if the student has arrived later than the school
+                 * start time. If they are, a new entry is created in the excuse
+                 * table
+                 */
+                if (getHour(logTime) > getHour(startTime)) {
+                    addToExcuses = true;
+                } else if (getHour(logTime) == getHour(startTime) && getMinute(logTime) > getMinute(startTime)) {
+                    addToExcuses = true;
+                }
+
+                /**
+                 * Check to see if addToExcuses variable is true, if it is, a
+                 * new entry is created in the excuses table and an email is
+                 * sent to the parent.
+                 */
+                if (addToExcuses == true) {
+                    /**
+                     * Retrieve attendanceID for the entry that we just created
+                     * in the attendance table.
+                     */
+                    ResultSet attendanceIDSet = statement.executeQuery("SELECT * FROM attendance WHERE studentID = " + studentID + " AND date = '" + logDate + "' AND signInTime = '" + logTime + "'");
+                    attendanceIDSet.next();
+                    int attendanceID = attendanceIDSet.getInt("attendanceID");
+
+                    addExcuse(attendanceID);
+                    sendEmail(tagKey);
+                }
+                return true;
+            } catch (SQLException ex) {
+                Logger.getLogger(SerialCommunication.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return false;
+        } else {
+            try {
+                /**
+                 * Retrieve attendanceID based on entries for the student where
+                 * there is no sign out time
+                 */
+                ResultSet attendanceIDSet = statement.executeQuery("SELECT * FROM attendance WHERE studentID = " + studentID + " AND signOutTime IS NULL");
+                attendanceIDSet.next();
+                int attendanceID = attendanceIDSet.getInt("attendanceID");
+                statement.execute("UPDATE attendance SET signOutTime = '" + logTime + "' WHERE  attendanceID = " + attendanceID);
+
+                /**
+                 * Check to see if the student has departed earlier than the
+                 * school finish time. If they have, a new entry is created in
+                 * the excuse table.
+                 */
+                if (getHour(logTime) < getHour(finishTime)) {
+                    addToExcuses = true;
+                } else if (getHour(logTime) == getHour(finishTime) && getMinute(logTime) < getMinute(finishTime)) {
+                    addToExcuses = true;
+                }
+
+                /**
+                 * Check to see if addToExcuses variable is true, if it is, a
+                 * new entry is created in the excuses table and an email is
+                 * sent to the parent.
+                 */
+                if (addToExcuses == true) {
+                    addExcuse(attendanceID);
+                    sendEmail(tagKey);
+                }
+                return true;
+            } catch (SQLException ex) {
+                Logger.getLogger(SerialCommunication.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return false;
+        }
+    }
+
+    /**
+     * This method creates a new entry in the excuse table based on the
+     * attendance number
+     *
+     * @param attendanceID the attendance number that the excuse relates to
+     * @return whether the entry was successful or not
+     */
+    public boolean addExcuse(int attendanceID) {
+        try {
+            /**
+             * Insert the new record into the excuses table.
+             */
+            statement.execute("INSERT INTO excuses (excuseID, attendanceID, verifiedByStaff, excuse)"
+                    + "VALUES (NULL, " + attendanceID + ", 0, NULL)");
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(SerialCommunication.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    /**
+     * This method will convert a time in sting format to a split string array
+     * then convert the first string(hour) to an integer. This value will be
+     * returned.
+     *
+     * @param time the time in string format
+     * @return an integer representation of the hour portion of the string time
+     */
+    public int getHour(String time) {
+        String[] arrayStringTime = time.split(":");
+        int hour = Integer.parseInt(arrayStringTime[0]);
+        return hour;
+    }
+
+    /**
+     * This method will convert a time in sting format to a split string array
+     * then convert the second string(minute) to an integer. This value will be
+     * returned.
+     *
+     * @param time the time in string format
+     * @return an integer representation of the minute portion of the string
+     * time
+     */
+    public int getMinute(String time) {
+        String[] arrayStringTime = time.split(":");
+        int minute = Integer.parseInt(arrayStringTime[0]);
+        return minute;
+    }
+
+    /**
+     * Sends an email to the parents of the student associated with the tagKey
+     * identified.
+     *
      * @param tagKey the tagKey read by an RFID reader
      */
     public void sendEmail(String tagKey) {
@@ -95,12 +258,15 @@ public class SerialCommunication {
             String subject = "Urgent! Please read";
             String message = "";
             if (present == 1) {
-                message = " arrived at school.";
+                message = " arrived at school late.";
             } else {
-                message = " departed school.";
+                message = " departed school early.";
             }
 
-            String body = "We wish to inform you that " + name + " has just " + message + "\nKind regards, \nThe RFID team";
+            String body = "We wish to inform you that " + name + " has just " + message + "\n "
+                    + "Please log into the web application to verify the matter. \n"
+                    + "Kind regards, \n"
+                    + "The RFID team";
 
             Mail mail = new Mail(email, subject, body);
         } catch (SQLException ex) {
@@ -109,8 +275,9 @@ public class SerialCommunication {
     }
 
     /**
-     * Runs 
-     * @param args 
+     * Runs
+     *
+     * @param args
      */
     public static void main(String args[]) {
 
